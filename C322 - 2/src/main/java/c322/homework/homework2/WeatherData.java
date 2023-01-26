9856a361729882629921a6a1c824e4e4421ed5ca
zorth <zorth@iu.edu>
2023-01-26 11:01:36 -0500

    prototype

--- C322 - 2/src/main/java/c322/homework/homework2/WeatherData.java
+++ C322 - 2/src/main/java/c322/homework/homework2/WeatherData.java
@@ -1,18 +1,42 @@
 package c322.homework.homework2;
+import java.util.Random;
 
 public class WeatherData {
+    float tempLimit = 100;
+    double humLimit = 100.0;
+    int lbPress = 100;
+    int upPress = 950;
+
+    Random randPress = new Random();
+    Random randTemp = new Random();
+    Random randHumid = new Random();
+    float useRandTemp = randTemp.nextFloat(tempLimit);
+    double useRandHum = randHumid.nextDouble(humLimit);
+    int useRandPress = randPress.nextInt(upPress) + lbPress;
 
     // These are getter methods that need to be implemented
-    void getTemperature (){
-        System.out.print(useRandTemp);
+    public float getTemperature() {
+        return useRandTemp;
     }
-    void getHumidity (){
+
+    public double getHumidity() {
+        return useRandHum;
+    }
+
+    public int getPressure() {
+        return useRandPress;
+    }
+
+    public void measurementsChanged() {
 
     }
-    void getPressure(){
 
-    }
-    public void measurementsChanged(){
+    public void setMeasurements(float useRandTemp, double useRandHum, int useRandPress) {
+        this.useRandHum = useRandHum;
+        this.useRandTemp = useRandTemp;
+        this.useRandPress = useRandPress;
 
     }
 }
+
+
--- C322 - 2/src/main/java/c322/homework/homework2/WeatherStation.java
+++ C322 - 2/src/main/java/c322/homework/homework2/WeatherStation.java
@@ -1,18 +1,9 @@
 package c322.homework.homework2;
-import java.util.Random;
 
 public class WeatherStation {
-    // We need to generate the actual weahter data here
-    float tempLimit = 100;
-    double humLimit = 100.0;
-    int lbPress = 100;
-    int upPress = 950;
+    public static void main(String[] args) {
+        WeatherData wd = new WeatherData();
 
-    Random randPress = new Random();
-    Random randTemp = new Random();
-    Random randHumid = new Random();
-    float useRandTemp = randTemp.nextFloat(tempLimit);
-    double useRandHum = randHumid.nextDouble(humLimit);
-    int useRandPress = randPress.nextInt(upPress ) + lbPress;
+    }
 
 }
