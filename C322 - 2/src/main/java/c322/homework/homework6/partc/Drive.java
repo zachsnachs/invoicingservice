b299cb8c217d599c0247e824253ea78d2998eb3c
zorth <zorth@iu.edu>
2023-02-24 23:51:25 -0500

    fixed null value bug

--- C322 - 2/src/main/java/c322/homework/homework6/partc/Drive.java
+++ C322 - 2/src/main/java/c322/homework/homework6/partc/Drive.java
@@ -2,6 +2,11 @@ package c322.homework.homework6.partc;
 
 public class Drive {
     public static void main(String[] args) {
+        Printer printer = new Printer();
 
+        System.out.println(printer);
+
+        printer.pushPrint();
     }
+
 }
--- C322 - 2/src/main/java/c322/homework/homework6/partc/Printer.java
+++ C322 - 2/src/main/java/c322/homework/homework6/partc/Printer.java
@@ -10,7 +10,29 @@ public class Printer {
 
     public Printer() {
         printerOnline = new PrinterOnline(this);
+        printerOffline = new PrinterOffline(this);
+        state = printerOffline;
     }
+
+    public void pushPrint(){
+        state.pushPrint();
+
+    }
+
+    void startJob(){
+        System.out.println("We're adding a job to the queue");
+        hasJob = true;
+    }
+
+    void endJob(){
+        System.out.println("We're ending the job, feel free to add another");
+        hasJob = false;
+    }
+
+    boolean getJob(){
+        return hasJob;
+    }
+
 
     void setState(PrinterState state) {
             this.state = state;
@@ -20,9 +42,12 @@ public class Printer {
         return state;
     }
 
-    boolean getJob(){
-        return hasJob;
-    }
 
+    public String toString() {
+        StringBuffer result = new StringBuffer();
+        result.append("\nPrinter Application");
+        result.append("\n. . . ");
+        return result.toString();
+    }
 
 }
