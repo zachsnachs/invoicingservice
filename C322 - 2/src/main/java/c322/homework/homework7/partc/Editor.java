793d096bbbca3a8371f6c2ef342d3d8c7aaff268
zorth <zorth@iu.edu>
2023-03-02 02:35:04 -0500

    tester

---
+++ C322 - 2/src/main/java/c322/homework/homework7/partc/Editor.java
@@ -0,0 +1,25 @@
+package c322.homework.homework7.partc;
+
+import java.io.File;
+
+public class Editor {
+    public EventManager events;
+    private File file;
+
+    public Editor() {
+        this.events = new EventManager("open", "save");
+    }
+
+    public void openFile(String filePath) {
+        this.file = new File(filePath);
+        events.notify("open", file);
+    }
+
+    public void saveFile() throws Exception {
+        if (this.file != null) {
+            events.notify("save", file);
+        } else {
+            throw new Exception("Error: Open a file first . . .");
+        }
+    }
+}
---
+++ C322 - 2/src/main/java/c322/homework/homework7/partc/ListenerTestDrive.java
@@ -0,0 +1,16 @@
+package c322.homework.homework7.partc;
+
+public class ListenerTestDrive {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.events.subscribe("open", new LogOpenListener("/path/to/log/file.txt"));
+        editor.events.subscribe("save", new EmailNotificationListener("admin@example.com"));
+
+        try {
+            editor.openFile("test.txt");
+            editor.saveFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
