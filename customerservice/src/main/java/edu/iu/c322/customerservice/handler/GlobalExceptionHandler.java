0eabf6eab97ba619705d22ac8d40af2dabfb1c3f
Zach <zorth@iu.edu>
2023-03-24 11:41:00 -0400

    fix

--- customerservice/src/main/java/edu/iu/c322/customerservice/handler/GlobalExceptionHandler.java
+++ customerservice/src/main/java/edu/iu/c322/customerservice/handler/GlobalExceptionHandler.java
@@ -3,8 +3,6 @@ package edu.iu.c322.customerservice.handler;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
@@ -17,8 +15,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> handleException(MethodArgumentNotValidException exception){
-        String errorMessage = exception.getBindingResult().getFieldError().stream().map(error -> error.getDefaultMessage()).collect(Collectors.joining(","));
-
-        return new ResponseEntity.badRequest().body(errorMessage);
+        String errorMessages = exception.getBindingResult().getFieldErrors().stream().map(error -> error.getDefaultMessage())
+                .collect(java.util.stream.Collectors.joining(", "));
+        return ResponseEntity.badRequest().body(errorMessages);
     }
 }
